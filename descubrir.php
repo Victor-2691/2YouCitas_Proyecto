3dbac083e9d91f3466fbb6411244b293564e205f
marcosh180816 <106186386+marcosh180816@users.noreply.github.com>
2023-03-09 15:41:35 -0600

    descubrir

---
+++ descubrir.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'includes/funciones.php';
+incluirTempleate('header_interno');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Descubrir</title>
+</head>
+<body>
+    <h1>Descubrir</h1>
+
+    <div class="card">
+        <div class="content">
+            <h2>Fabiola Villalobos, 24</h2>
+            <a href="#">Ver Perfil </a>
+        </div>
+    </div>
+</body>
+
+<?php
+incluirTempleate('footer');
+?>
+</html>
---
+++ includes/templates/header_interno.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2 YouCitas</title>
+    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <script src="https://kit.fontawesome.com/474eae7f54.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <!-- Para solo mostrar el header en la pagina de inicio
+So es true mostramos el header inicio que tiene la imagen -->
+    <header class="header">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a class="animate__animated animate__rubberBand" href="/Proyectos/
+                bienesRaicesPHP_inicio/index.php">
+                    <img src="build/img/logo2youcitas150final.svg" alt="Logotipo 2YouCitas">
+                </a>
+                <div class="mobile-menu">
+                    <img src="build/img/barras.svg" alt="icono menu responsive">
+                </div>
+
+                <div class="row center-xs margin-top">
+                    <div class="col-xs-6">
+                        <div class="box">
+                        <i class="fa-brands fa-product-hunt fa-beat "></i>
+                        <i class="fa-brands fa-product-hunt fa-beat "></i>
+                        <i class="fa-brands fa-product-hunt fa-beat "></i>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="derecha">
+                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
+                    <nav class="navegacion">
+                        <a href="#">Descubrir</a>
+                        <a href="#">Mensajes</a>
+                        <a href="#">Actividad</a>
+                        <a href="#">Perfil</a>
+                        <a href="#">Salir</a>
+                    </nav>
+                </div>
+
+            </div> <!--.barra-->
+
+
+
+
+
+
+        </div>
+    </header>
