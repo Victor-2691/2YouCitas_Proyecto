837e3556d0499761bb22a5f82b63879b970a528f
marcosh180816 <106186386+marcosh180816@users.noreply.github.com>
2023-03-13 19:47:58 -0600

    login funcional

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+
+ $db = mysqli_connect('138.59.135.31', 'tiusr2cp_grupo4', ')vyEXe_G.IhT', 'tiusr2cp_2youcitasbd_sitiosg4');
+if(!$db){
+    echo "Error en la conexion";
+}
+else{
+    echo "Conexion exitosa remota";
+}
+
+
+?>
--- inicio_sesion.php
+++ inicio_sesion.php
@@ -1,4 +1,6 @@
-
+<?php
+include_once 'db.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,14 +18,15 @@
                 <div class="cine-wrap-form">
                     <h2><span class="txt-primary">2You</span>Citas</h2>
                     <h3>Inicio Sesion</h3>
-                    <form action="" class="form 
+                    <form action="" method="POST" class="form 
                     form-ln-gray">
                         <label for="email">Email</label>
-                        <input type="email" id="email">
+                        <input type="email" id="email" name="email">
                         <label for="password">Contraseña</label>
-                        <input type="password">
+                        <input type="password" name="password">
                         <p class="small txt-gray"><a href="#"><span class="txt-primary">Olvide mi Contraseña</span></a></p>
-                        <button class="btn" type="button" onclick="location.href='descubrir.php'">Iniciar Sesion</button>
+                        <input class="btn" type="submit" name="iniciar_sesion" value="Iniciar Sesion">
+                        <!--<button class="btn" type="button" onclick="location.href='descubrir.php'">Iniciar Sesion</button> -->
                     </form>
                     <p class="small">
                         Registrarse <a href="#"><span class="txt-primary">Aqui</span></a>
@@ -34,6 +37,40 @@
     </section>
 </body>
 </html>
+
+<?php 
+
+session_start();
+if (isset($_SESSION['nombredelusuario'])) {
+    header('location: descubrir.php');
+}
+
+
+            //este iniciar_sesion es el boton de la pagina
+if (isset($_POST['iniciar_sesion'])) {
+
+    $correo = $_POST['email'];
+    $pass = $_POST['password'];
+
+    $query = mysqli_query($db,"SELECT * FROM Usuarios_Clientes_Externo WHERE correo_electronico = '".$correo."' AND contrasena = '".$pass."' ");
+    //$querynombre = mysqli_query($db,"SELECT * FROM Clientes_Externos WHERE nombre = '".$nombre."' ");
+    //esta  variable es para contar las filas del query
+    $nr = mysqli_num_rows($query);
+
+    //validar que no hayan 2 usuarios ingresando al mismo tiempo
+    if (!isset($_SESSION['nombredelusuario'])) {
+
+        if ($nr == 1) {
+            $_SESSION['nombredelusuario'] = $correo;
+            header("location: descubrir.php");
+        }elseif ($nr == 0) {
+            echo "<script>alert('El correo no existe'); window.location = 'inicio_sesion.php' </script>";
+        }
+    }
+
+}
+
+?>
 
 
 
